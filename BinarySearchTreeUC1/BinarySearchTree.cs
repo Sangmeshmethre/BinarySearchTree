4a6c02b0d3936193dc81db1c7c93fb7a1c844b40
Sangmesh Methre <SangmeshMetre>
2022-11-09 18:55:56 +0530

    AddingBSTNumber

---
+++ BinarySearchTreeUC1/BinarySearchTree.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeUC1
+{
+    //Generic class where T is IComparable
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+
+
+        //creating get set method for lefttree and righttree
+        public BinarySearchTree<T> leftTree { get; set; }
+        public BinarySearchTree<T> rightTree { get; set; }
+
+        public BinarySearchTree(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.rightTree = null;
+            this.leftTree = null;
+
+        }
+
+        //defining variables
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+
+        //Creating Add method where parameter as T 
+
+        public void Add(T item)
+        {
+            T root = this.NodeData;
+
+            if (root.CompareTo(item) > 0)
+            {
+                if (this.leftTree == null)
+                {
+                    this.leftTree = new BinarySearchTree<T>(item);
+
+                }
+                else
+                    this.leftTree.Add(item);
+            }
+            else
+            {
+                if (this.rightTree == null)
+                {
+                    this.rightTree = new BinarySearchTree<T>(item);
+                }
+                else
+                    this.rightTree.Add(item);
+            }
+        }
+
+        //Display method to add elements in respective position
+
+        public void Display()
+        {
+            Console.WriteLine(this.NodeData.ToString());
+
+            if (this.leftTree != null)
+            {
+                this.leftCount++;
+                Console.Write("On Left Tree :");
+                this.leftTree.Display();
+            }
+
+            if (this.rightTree != null)
+            {
+                this.rightCount++;
+                Console.Write("On Right Tree :");
+                this.rightTree.Display();
+            }
+        }
+    }
+}
---
+++ BinarySearchTreeUC1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeUC1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree Program");
+
+            bool check = true;
+
+
+            Console.WriteLine("Select Options :\n" +
+                "1)Enter elements in the Binary Search Tree\n");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                    BinarySearchTree<int> binary = new BinarySearchTree<int>(56);        //56 taken as a root element
+                    binary.Add(30);
+                    binary.Add(70);
+                    Console.WriteLine("::::::::::::::::::::::::::::::::::");
+                    Console.Write("Root Node is : ");
+                    binary.Display();
+                    break;
+
+                default:
+
+                    Console.WriteLine("Please enter Proper option");
+                    break;
+            }
+        }
+    }
+}
